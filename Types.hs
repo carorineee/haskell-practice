5dbc459ce7e473703d44b7573ae4ea31f9b52d62
Caroline Shi <carolineshi97@gmail.com>
2020-02-05 01:37:47 -0800

    Added type and record syntax

--- Types.hs
+++ Types.hs
@@ -1,3 +1,19 @@
 module Types (
-
+  Point(..),
+  Shape(..),
+  area,
 ) where
+
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rect Point Point deriving (Show)
+
+area :: Shape -> Float
+area (Circle _ r) = 2 * pi * r
+area (Rect (Point x1 y1) (Point x2 y2)) = abs (y2 - y1) * abs (x2 - x1)
+
+-- Record syntax
+data Person = Person {
+  first :: String,
+  last :: String,
+  age :: Int
+} deriving (Show)
